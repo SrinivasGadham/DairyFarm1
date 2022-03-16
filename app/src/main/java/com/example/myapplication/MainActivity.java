7ce207c40889798b30264bc89dd9631bf286af5a
gadamsrinivasyadav7 <gadamsrinivasyadav7@gmail.com>
2022-03-16 12:05:40 +0530

    one commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.view.View;
+
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.auth.FirebaseUser;
+
+import com.google.firebase.*;
+
+import javax.security.auth.login.LoginException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private Button btnLogout;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogout = findViewById(R.id.btnlogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                logout();
+
+            } }); }
+
+    @Override
+
+    public void onStart(){
+
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser==null)
+
+        { startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+        } }
+
+    public void logout() {
+
+        FirebaseAuth.getInstance().signOut();
+
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+    }}
